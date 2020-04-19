6e0c30723fbbe35e85e97a53bab6cd00d083e871
EliBrus <eli_brus93@live.it>
2020-04-19 23:44:47 +0200

    Aggiunto fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UsersComponent } from './users/users.component';
-
 import { UserService } from "./services/user.service";
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NavComponent } from './nav/nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UsersComponent, UserComponent, UserDetailComponent ],
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
+  declarations: [ AppComponent, HelloComponent, UsersComponent, UserComponent, UserDetailComponent, NavComponent ],
   providers: [ UserService ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { User } from "../classes/user"; 
+import { User } from "../classes/user";
+import { faAngular, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+import { faBook, faPencil } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'tr[app-user]',
@@ -8,6 +10,10 @@ import { User } from "../classes/user";
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  faAngular = faAngular;
+  faFontAwesome = faFontAwesome;
+  faBook = faBook;
+  faPencil = faPencil;
   @Input('user-data') user: User;
   @Output ('onDeleteUser') onDeleteUser = new EventEmitter();
   @Output ('onSelectUser') onSelectUser = new EventEmitter();
